1c226b44cf2d156c42ab06312fc98113916d0549
Chloe Baron <chloe.f.baron@gmail.com>
2018-05-16 11:38:03 -0400

    Framework set up

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,11 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap-sprockets
+
+// app/assets/javascripts/application.js
+
+
